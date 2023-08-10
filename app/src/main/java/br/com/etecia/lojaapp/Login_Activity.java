61d0b83dba4c53e7f1798e2e37e246b0e53e7576
joao <joaoguilhermehig@gmail.com>
2023-08-10 20:01:15 -0300

    JANELAS CRIADAS

--- app/src/main/java/br/com/etecia/lojaapp/Login_Activity.java
+++ app/src/main/java/br/com/etecia/lojaapp/Login_Activity.java
@@ -1,13 +1,14 @@
 package br.com.etecia.lojaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
     }
 }
--- app/src/main/java/br/com/etecia/lojaapp/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/lojaapp/Splash_Activity.java
@@ -3,11 +3,11 @@ package br.com.etecia.lojaapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_layout);
     }
 }
