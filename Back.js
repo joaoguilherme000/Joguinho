a8fe75bd1a9360626829cf3365dfee0495993bcd
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-19 18:26:16 -0300

    jquery meio feito

--- Back.js
+++ Back.js
@@ -1,6 +1,7 @@
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 const winner = document.getElementById("winner");
+const chegada = document.getElementById("chegada");
 
 document.addEventListener("keydown", function (event) {
     if (event.key === "0") {
@@ -14,8 +15,12 @@ document.addEventListener("keydown", function (event) {
 function movePlayer(player, distance) {
     const currentPosition = parseInt(player.style.left) || 0;
     player.style.left = currentPosition + distance + "px";
+    const maxWidth = window.innerWidth - player.offsetWidth; // Define a posição máxima
     
-    if (currentPosition + distance >= window.innerWidth - 50) {
+    if (currentPosition + distance < maxWidth) {
+        player.style.left = currentPosition + distance + "px";
+    } else {
+        player.style.left = maxWidth + "px"; // Define a posição máxima se o limite for atingido
         declareWinner(player);
     }
 }
